b01727c4f4227dc783ad07b444c2452ef00174f6
AugustoGirotti <agugirotti@gmail.com>
2021-11-22 13:31:59 -0300

    testing

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -1,22 +1,52 @@
 const { Country, conn } = require('../../src/db.js');
 const { expect } = require('chai');
+const db = require('../../src/db.js');
 
-describe('Country model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
-    beforeEach(() => Country.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Country.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
-      });
-      it('should work when its a valid name', () => {
-        Country.create({ name: 'Argentina' });
-      });
-    });
-  });
-});
+// describe('Country model', () => {
+//   before(() => conn.authenticate()
+//     .catch((err) => {
+//       console.error('Unable to connect to the database:', err);
+//     }));
+//   describe('Validators', () => {
+//     beforeEach(() => Country.sync({ force: true }));
+//     describe('name', () => {
+//       it('should throw an error if name is null', (done) => {
+//         Country.create({})
+//           .then(() => done(new Error('It requires a valid name')))
+//           .catch(() => done());
+//       });
+//       it('should work when its a valid name', () => {
+//         Country.create({ name: 'Argentina' });
+//       });
+//     });
+    
+//   });
+// });
+
+describe('Country testing', () => {
+  after(async function(){
+    await conn.sync({force: true});
+    // conn.close();
+  })
+  describe('Country model', function(){
+    beforeEach(async function() {
+      await Country.sync({force:true})
+    })
+    describe('Validaciones', function(){
+      it ('Error sin nombre', function(done){
+        Country.create({
+          id:'ARG'
+        })
+        .then(() => done('No deberia haberse creado'))
+        .catch(() => done())
+      })
+      it('error sin id', function(done){
+        Country.create({
+          name:'Argentina'
+        })
+        .then(() => done('no deberia haberse creado'))
+        .catch(() => done())
+      })
+    })
+  })
+})
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,12 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { expect } = require('chai');
+// const { DESCRIBE } = require('sequelize/types/lib/query-types');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Country, conn } = require('../../src/db.js');
 
 const agent = session(app);
+
+
 const country = {
   name: 'Argentina',
+  id: "ARG",
+  image: "https://flagcdn.com/ar.svg",
+  continent:"Americas" ,
+  capital: "Buenos Aires",
+  subregion:null ,
+  area:null ,
+  population:null, 
+  // activities:[]
 };
 
 describe('Country routes', () => {
@@ -14,11 +25,18 @@ describe('Country routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
+  after(() => conn.close())
   beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
+    .then(() => Country.create(country)));
   describe('GET /countries', () => {
     it('should get 200', () =>
       agent.get('/countries').expect(200)
     );
+
+    it('responds with an object with the country', () =>
+    agent.get('/countries').then((res) => {
+      expect(res.body[0].name).to.be.equal('Argentina');
+    }));
   });
 });
+
